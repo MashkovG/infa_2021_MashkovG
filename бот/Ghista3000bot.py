1ea146ae2268a890158eb50418cb4662b9f0c5d8
MashkovG <72259379+MashkovG@users.noreply.github.com>
2021-05-17 19:04:46 +0300

    телеграм бот
    помогает учить гистологию

---
+++ бот/Ghista3000bot.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import telegram
+import config
+TOKEN = config.token
+bot = telegram.Bot(TOKEN)
+
---
+++ бот/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+token = '1875094773:AAGsGXWgGUsqOPWPXa6UOJFzMQg3aBYLvHE'
+
